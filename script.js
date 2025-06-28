b0c2012c2983b57c013967d6630fcf419ef854c1
cillb# <cillian.boland.1998@gmail.com>
2025-06-28 14:58:52 -0400

    Added DOM elements. Declared function components.

---
+++ script.js
@@ -0,0 +1,33 @@
+const searchBox = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+const creName = document.getElementById("creature-name");
+const creId = document.getElementById("creature-id");
+const creTypes = document.getElementById("types");
+
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specAttack = document.getElementById("special-attack");
+const specDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+
+const validateSearch = () => {}
+
+const search = () => {}
+
+const clearTypes = () => {}
+
+const updateCreatureBlock = () => {}
+
+const updateStatsBlock = () => {}
+
+const animateStats = () => {}
+
+searchButton.addEventListener("click", () => {});
+
+searchBox.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+        
+    }
+});
